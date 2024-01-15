bee6fec02e8f8ecc76fc1339cc2354d99176470e
RendyAFS <arendy724@gmail.com>
2024-01-15 19:02:27 +0700

    fix select in Mobile

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar shadow">
     <div class="container-fluid">
         <a class="navbar-brand text-white fw-bold" href="#">Connect Care Pediatrics</a>
-        <button class="navbar-toggler text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <button class="navbar-toggler text-white" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <i class='bx bx-menu fs-2'></i>
         </button>
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
@@ -12,55 +12,49 @@
             </div>
             <div class="offcanvas-body">
                 <ul class="nav-list">
-                    <li>
-                        <a href="{{ route('bidans.index') }}">
+                    <a href="{{ route('bidans.index') }}">
+                        <li>
                             <i class='bx bx-grid-alt'></i>
-                            <span class="links_name">Dashboard</span>
-                        </a>
-                        <span class="tooltip">Dashboard</span>
-                    </li>
-                    <li>
-                        <a href="{{ route('danaks.index') }}">
+                            <span>Dashboard</span>
+                        </li>
+                    </a>
+                    <a href="{{ route('danaks.index') }}">
+                        <li>
                             <i class='bx bx-user'></i>
-                            <span class="links_name">Data Anak</span>
-                        </a>
-                        <span class="tooltip">Data Anak</span>
-                    </li>
-                    <li>
-                        <a href="{{ route('dposyandus.index') }}">
+                            <span>Data Anak</span>
+                        </li>
+                    </a>
+                    <a href="{{ route('dposyandus.index') }}">
+                        <li>
                             <i class='bx bx-building-house'></i>
-                            <span class="links_name">Data Posyandu</span>
-                        </a>
-                        <span class="tooltip">Data Posyandu</span>
-                    </li>
-                    <li>
-                        <a href="{{ route('dbulanans.index') }}">
+                            <span>Data Posyandu</span>
+                        </li>
+                    </a>
+                    <a href="{{ route('dbulanans.index') }}">
+                        <li>
                             <i class='bx bx-table'></i>
-                            <span class="links_name">Data Bulanan</span>
-                        </a>
-                        <span class="tooltip">Data Bulanan</span>
-                    </li>
-                    <li>
-                        <a href="{{ route('gperkembangans.index') }}">
+                            <span>Data Bulanan</span>
+                        </li>
+                    </a>
+                    <a href="{{ route('gperkembangans.index') }}">
+                        <li>
                             <i class='bx bx-line-chart'></i>
-                            <span class="links_name">Grafik Perkembangan</span>
-                        </a>
-                        <span class="tooltip">Grafik Perkembangan</span>
-                    </li>
+                            <span>Grafik Perkembangan</span>
+                        </li>
+                    </a>
 
-                    <li class="profile" id="btn-logout">
-
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                                                 document.getElementById('logout-form').submit();">
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                        <li class="profile" id="btn-logout">
 
                             <i class='bx bx-log-out out' id="log_out"></i>
                             Logout
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+                    </a>
                 </ul>
 
             </div>
