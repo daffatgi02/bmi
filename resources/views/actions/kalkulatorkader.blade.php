e23653ddc695ebc1728d87950a245577f658413f
RendyAFS <arendy724@gmail.com>
2024-02-15 20:49:39 +0700

    update IOT-FIrebase

--- resources/views/actions/kalkulatorkader.blade.php
+++ resources/views/actions/kalkulatorkader.blade.php
@@ -8,28 +8,34 @@
         <div class="card-body">
             <div class="d-flex ">
                 <i class="bi bi-search fs-3 me-2"></i>
-                <input type="text" id="searchInput" class="form-control mb-3 border border-dark-subtle" placeholder="Cari Nama" onclick="selectAllText(this);" onfocus="selectAllText(this);" required>
+                <input type="text" id="searchInput" class="form-control mb-3 border border-dark-subtle"
+                    placeholder="Cari Nama" onclick="selectAllText(this);" onfocus="selectAllText(this);" required>
             </div>
             <div class="row">
                 <div class="col-12">
-                    <select class="form-select mb-2 border border-dark-subtle" id="danaks_id" name="danaks_id" required style="cursor: pointer">
+                    <select class="form-select mb-2 border border-dark-subtle" id="danaks_id" name="danaks_id" required
+                        style="cursor: pointer">
                         <option class="fw-bold fs-5 mb-3 text-center bg-dark-subtle rounded-2" value="null">
                             Silahkan Pilih Nama
                         </option>
 
                         @foreach ($danaks->sortBy('nama_anak') as $data)
-                        @php
-                        $tanggal_lahir = new DateTime($data->tanggal_lahir);
-                        $sekarang = new DateTime();
-                        $umur = $tanggal_lahir->diff($sekarang);
+                            @php
+                                $tanggal_lahir = new DateTime($data->tanggal_lahir);
+                                $sekarang = new DateTime();
+                                $umur = $tanggal_lahir->diff($sekarang);
 
-                        $umurTotal = $umur->y;
-                        $umurTotal2 = $umur->m;
-                        $umurTotal3 = $umur->y . ' Tahun ' . $umur->m . ' Bulan';
-                        @endphp
-                        <option class="border border-dark-subtle mb-2 px-2 overflow-auto overflow-md-hidden" value="{{ $data->id }}" {{-- untuk rumus stunting --}} data-jk="{{ $data->jk }}" data-nama_posyandu="{{ $data->dposyandu->nama_posyandu }}" data-umur="{{ $umurTotal }}" data-umur2="{{ $umurTotal2 }}" data-umur3="{{ $umurTotal3 }}"> <!-- Menambahkan data-umur -->
-                            {{ $data->nama_anak }} | {{ $data->nik_anak }}
-                        </option>
+                                $umurTotal = $umur->y;
+                                $umurTotal2 = $umur->m;
+                                $umurTotal3 = $umur->y . ' Tahun ' . $umur->m . ' Bulan';
+                            @endphp
+                            <option class="border border-dark-subtle mb-2 px-2 overflow-auto overflow-md-hidden"
+                                value="{{ $data->id }}" {{-- untuk rumus stunting --}} data-jk="{{ $data->jk }}"
+                                data-nama_posyandu="{{ $data->dposyandu->nama_posyandu }}"
+                                data-umur="{{ $umurTotal }}" data-umur2="{{ $umurTotal2 }}"
+                                data-umur3="{{ $umurTotal3 }}"> <!-- Menambahkan data-umur -->
+                                {{ $data->nama_anak }} | {{ $data->nik_anak }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
@@ -39,10 +45,12 @@
                 <div class="mb-3 d-flex justify-content-center align-items-center">
                     <form>
                         <div class="form-check form-check-inline">
-                            <button class="btn border border-dark" type="button" id="berdiriButton" value="Berdiri">Berdiri</button>
+                            <button class="btn border border-dark" type="button" id="berdiriButton"
+                                value="Berdiri">Berdiri</button>
                         </div>
                         <div class="form-check form-check-inline">
-                            <button class="btn border border-dark" type="button" id="telentangButton" value="Telentang">Telentang</button>
+                            <button class="btn border border-dark" type="button" id="telentangButton"
+                                value="Telentang">Telentang</button>
                         </div>
                     </form>
                 </div>
@@ -63,10 +71,14 @@
                     </div>
                 </div>
 
-                <input type="text" id="umur_tahun" name="umur_tahun" class="d-none " placeholder="umur_tahun" required>
-                <input type="text" id="umur_bulan" name="umur_bulan" class="d-none " placeholder="umur_bulan" required>
-                <input type="text" id="umur_periksa" name="umur_periksa" class="d-none" required placeholder="umur_periksa">
-                <input type="text" id="nama_posyandu" name='nama_posyandu' class="d-none" required placeholder="nama_posyandu">
+                <input type="text" id="umur_tahun" name="umur_tahun" class="d-none " placeholder="umur_tahun"
+                    required>
+                <input type="text" id="umur_bulan" name="umur_bulan" class="d-none " placeholder="umur_bulan"
+                    required>
+                <input type="text" id="umur_periksa" name="umur_periksa" class="d-none" required
+                    placeholder="umur_periksa">
+                <input type="text" id="nama_posyandu" name='nama_posyandu' class="d-none" required
+                    placeholder="nama_posyandu">
                 <input type="text" id="c_ukur" name='c_ukur' class="d-none" required placeholder="c_ukur">
                 <input type="text" id="h_imt" name='h_imt' class="d-none" required placeholder="h_imt">
 
@@ -75,14 +87,18 @@
                     {{-- Untuk Umur Periksa --}}
 
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control border border-dark-subtle  " id="bb_anak" name="bb_anak" placeholder="Masukan Berat Badan (KG)" value="0" required onclick="selectAllText(this);" onfocus="selectAllText(this);">
+                        <input type="text" class="form-control border border-dark-subtle  " id="bb_anak"
+                            name="bb_anak" placeholder="Masukan Berat Badan (KG)" value="0" required
+                            onclick="selectAllText(this);" onfocus="selectAllText(this);">
                         <label class="d-md-block d-none" for="bb_anak">Berat Badan (KG)</label>
                         <label class="d-md-none d-block" for="bb_anak">Berat Badan</label>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control border border-dark-subtle " id="tb_anak" name="tb_anak" placeholder="Masukan Tinggi Badan (KG)" value="0" required onclick="selectAllText(this);" onfocus="selectAllText(this);">
+                        <input type="text" class="form-control border border-dark-subtle " id="tb_anak"
+                            name="tb_anak" placeholder="Masukan Tinggi Badan (KG)" value="0" required
+                            onclick="selectAllText(this);" onfocus="selectAllText(this);">
                         <label class="d-md-block d-none" for="tb_anak">Tinggi Badan (CM)</label>
                         <label class="d-md-none d-block" for="tb_anak">Tinggi Badan</label>
                     </div>
@@ -90,7 +106,9 @@
                 {{-- LIla --}}
                 <div class="col-6">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control border border-dark-subtle  " id="lk_anak" name="lk_anak" placeholder="Masukan Lingkar Kepala (CM)" value="0" required onclick="selectAllText(this);" onfocus="selectAllText(this);">
+                        <input type="text" class="form-control border border-dark-subtle  " id="lk_anak"
+                            name="lk_anak" placeholder="Masukan Lingkar Kepala (CM)" value="0" required
+                            onclick="selectAllText(this);" onfocus="selectAllText(this);">
                         <label class="d-md-block d-none" for="lk_anak">Lingkar Kepala
                             (CM)</label>
                         <label class="d-md-none d-block" for="lk_anak">Lingkar Kepala</label>
@@ -98,7 +116,9 @@
                 </div>
                 <div class="col-6">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control border border-dark-subtle " id="ll_anak" name="ll_anak" placeholder="Masukan Lingkar Lengan (CM)" value="0" required onclick="selectAllText(this);" onfocus="selectAllText(this);">
+                        <input type="text" class="form-control border border-dark-subtle " id="ll_anak"
+                            name="ll_anak" placeholder="Masukan Lingkar Lengan (CM)" value="0" required
+                            onclick="selectAllText(this);" onfocus="selectAllText(this);">
                         <label class="d-md-block d-none" for="ll_anak">Lingkar Lengan
                             (CM)</label>
                         <label class="d-md-none d-block" for="ll_anak">Lingkar Lengan</label>
@@ -106,7 +126,8 @@
                 </div>
                 <div class="col-12">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control border-dark-subtle" id="st_anak" name="st_anak" placeholder="Status Anak" required readonly value="- Silahkan Masukan Data">
+                        <input type="text" class="form-control border-dark-subtle" id="st_anak" name="st_anak"
+                            placeholder="Status Anak" required readonly value="- Silahkan Masukan Data">
                         <label for="st_anak">Status Aanak</label>
                     </div>
                 </div>
@@ -126,7 +147,8 @@
                 <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
                     <div class="toast-header bg-warning">
                         <strong class="me-auto">Info</strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        <button type="button" class="btn-close" data-bs-dismiss="toast"
+                            aria-label="Close"></button>
                     </div>
                     <div class="toast-body bg-white">
                         Silahkan Pilih Cara Ukur Terlebih Dahulu
@@ -135,9 +157,10 @@
             </div>
         </div>
     </form>
-    <button onclick="getDataFromFirebase()" class="btn btn-outline-success" style="padding: 10px; margin: 0 auto 20px; width: 200px; display: block;">
-    <label class="fw-bold">UKUR DENGAN ALAT</label>
-</button>
+    <button onclick="getDataFromFirebase()" class="btn btn-outline-success"
+        style="padding: 10px; margin: 0 auto 20px; width: 200px; display: block;">
+        <label class="fw-bold pe-none">UKUR DENGAN ALAT</label>
+    </button>
 
 </div>
 <!-- Bootstrap JS (popper.js and bootstrap.js) -->
@@ -164,40 +187,40 @@
 
     //fungsi get data firebase
     function getDataFromFirebase() {
-    const toast = new bootstrap.Toast(document.getElementById('liveToast'));
-    const cUkur = document.getElementById('c_ukur').value;
+        const toast = new bootstrap.Toast(document.getElementById('liveToast'));
+        const cUkur = document.getElementById('c_ukur').value;
 
-    if (cUkur === 'Berdiri' || cUkur === 'Telentang') {
-        // Ambil data dari Firebase kalau di trigger button
-        timbanganRef.once('value')
-            .then((snapshot) => {
-                const data = snapshot.val();
-                if (data) {
-                    const berat = data.Berat || 0;
-                    const tinggi = data.Tinggi || 0;
-                    const panjang = data.Panjang || 0;
+        if (cUkur === 'Berdiri' || cUkur === 'Telentang') {
+            // Ambil data dari Firebase kalau di trigger button
+            timbanganRef.once('value')
+                .then((snapshot) => {
+                    const data = snapshot.val();
+                    if (data) {
+                        const berat = data.Berat || 0;
+                        const tinggi = data.Tinggi || 0;
+                        const panjang = data.Panjang || 0;
 
-                    if (cUkur === 'Berdiri') {
-                        document.getElementById('bb_anak').value = berat;
-                        document.getElementById('tb_anak').value = tinggi;
-                    } else if (cUkur === 'Telentang') {
-                        document.getElementById('bb_anak').value = berat;
-                        document.getElementById('tb_anak').value = panjang;
+                        if (cUkur === 'Berdiri') {
+                            document.getElementById('bb_anak').value = berat;
+                            document.getElementById('tb_anak').value = tinggi;
+                        } else if (cUkur === 'Telentang') {
+                            document.getElementById('bb_anak').value = berat;
+                            document.getElementById('tb_anak').value = panjang;
+                        }
+                        //debug di console
+                        //console.log('Data dari Firebase berhasil diambil:', { berat, tinggi, panjang });
+                    } else {
+                        console.error('Data dari Firebase kosong.');
                     }
-                    //debug di console
-                    //console.log('Data dari Firebase berhasil diambil:', { berat, tinggi, panjang });
-                } else {
-                    console.error('Data dari Firebase kosong.');
-                }
-            })
-            .catch((error) => {
-                console.error('Error fetching data from Firebase:', error);
-            });
-    } else {
-        // kalau si kader belum pilih cara ukur muncul notif toast
-        toast.show();
+                })
+                .catch((error) => {
+                    console.error('Error fetching data from Firebase:', error);
+                });
+        } else {
+            // kalau si kader belum pilih cara ukur muncul notif toast
+            toast.show();
+        }
     }
-}
 
 
     // Cara Ukur
---
+++ resources/views/ibubalita/ekms.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.appbalita')
+
+@section('content')
+    <div class="container mt-2 mb-5">
+        <p>
+            Proses Pembuatan
+        </p>
+    </div>
+@endsection
+
